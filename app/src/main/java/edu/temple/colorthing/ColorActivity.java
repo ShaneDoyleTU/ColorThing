f8a6b33468b242d1588fc10953c2c7f7362400f2
Shane Doyle <tug02508@temple.edu>
2019-02-18 18:49:47 -0500

    Hopefully this works.

--- app/src/main/java/edu/temple/colorthing/ColorActivity.java
+++ app/src/main/java/edu/temple/colorthing/ColorActivity.java
@@ -4,42 +4,27 @@ import android.graphics.Color;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.content.Context;
 
 public class ColorActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     //String[] colorlist;
-    @Override
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_color);
-
-        Spinner spinner = findViewById(R.id.spinner);
-        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.colors, android.R.layout.simple_spinner_item);
-        ColorAdaprer adapter = new ColorAdapter(this, R.array.colors);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
-
-        //colorlist = getResources().getStringArray(R.array.colors);
-
-        //RelativeLayout current = (RelativeLayout) findViewById(R.id.background);
-        //current.setBackgroundColor(Color.RED);
-
-    }
     class ColorAdapter extends BaseAdapter{
         Context context;
         String[] colors;
         public ColorAdapter(Context context,int arrayid){
             this.context = context;
-            colors = context.GetStringArray(arrayid)
+            colors = context.getResources().getStringArray(arrayid);
         }
         @Override
         public int getCount() {
@@ -66,20 +51,67 @@ public class ColorActivity extends AppCompatActivity implements AdapterView.OnIt
         @Override
         public View getDropDownView(int position, View convertView, ViewGroup parent){
             //put if else for convertView
+            //LinearLayout layout;
             TextView tv;
+
             if(convertView!=null){
+                //layout = (LinearLayout) convertView;
                 tv = (TextView) convertView;
             }
             else{
+                //layout = new LinearLayout(context);
+                //LayoutInflater l = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                //convertView = l.inflate(android.R.layout.simple_spinner_dropdown_item,parent,false);
                 tv = new TextView(context);
             }
+
+
+            //layout.setOrientation(LinearLayout.VERTICAL);
             tv.setText(colors[position]);
-            tv.setBackGroundColor(Color.parseColor(colors[position]);
+            if(colors[position].equals("DKGRAY")){
+                tv.setBackgroundColor(-12303292);
+            }
+            else {
+                tv.setBackgroundColor(Color.parseColor(colors[position]));
+            }
+            //layout.addView(tv);
             //String color = parent.getItemAtPosition(position).toString();
             //tv.setBackgroundColor(Color.parseColor(color));
             return tv;
+
         }
+
+        /*private View initView(int position, View convertView, ViewGroup parent){
+            if(convertView == null){
+                convertView = LayoutInflater.from(context).inflate(android.R.layout.simple_spinner_dropdown_item, parent, false);
+            }
+            TextView tv = convertView.
+            return convertView;
+        }*/
+
+
     }
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.colors, android.R.layout.simple_spinner_item);
+        ColorAdapter adapter = new ColorAdapter(this, R.array.colors);
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //adapter.initView()
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        //colorlist = getResources().getStringArray(R.array.colors);
+
+        //RelativeLayout current = (RelativeLayout) findViewById(R.id.background);
+        //current.setBackgroundColor(Color.RED);
+
+    }
+
 
 
     @Override
@@ -91,8 +123,13 @@ public class ColorActivity extends AppCompatActivity implements AdapterView.OnIt
         //view.setBackgroundColor(Color.parseColor(parent.getItemAtPosition(position).toString()));
         String color = parent.getItemAtPosition(position).toString();
         View back = findViewById(R.id.background);
-        back.setBackgroundColor(Color.parseColor(color));
-        parent.setBackgroundColor(Color.WHITE);
+        if(color.equals("DKGRAY")){
+            back.setBackgroundColor(-12303292);
+        }
+        else {
+            back.setBackgroundColor(Color.parseColor(color));
+        }
+        //parent.setBackgroundColor(Color.WHITE);
 
     }
 
