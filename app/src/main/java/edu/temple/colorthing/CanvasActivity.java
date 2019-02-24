afe774dddc2d6ba51de466043a089c3b6e4e08ef
Shane Doyle <tug02508@temple.edu>
2019-02-24 15:23:16 -0500

    Canvas Activity is called up with spanish colors now.

--- app/src/main/java/edu/temple/colorthing/CanvasActivity.java
+++ app/src/main/java/edu/temple/colorthing/CanvasActivity.java
@@ -19,6 +19,33 @@ public class CanvasActivity extends AppCompatActivity {
         if(color.equals("DKGRAY")){
             back.setBackgroundColor(-12303292);
         }
+        else if(color.equals("ROJO")){
+            back.setBackgroundColor(Color.parseColor("RED"));
+        }
+        else if(color.equals("AZUL")){
+            back.setBackgroundColor(Color.parseColor("BLUE"));
+        }
+        else if(color.equals("VERDE")){
+            back.setBackgroundColor(Color.parseColor("GREEN"));
+        }
+        else if(color.equals("GRISOSCURO")){
+            back.setBackgroundColor(-12303292);
+        }
+        else if(color.equals("CIAN")){
+            back.setBackgroundColor(Color.parseColor("CYAN"));
+        }
+        else if(color.equals("GRIS")){
+            back.setBackgroundColor(Color.parseColor("GRAY"));
+        }
+        else if(color.equals("AMARILLO")){
+            back.setBackgroundColor(Color.parseColor("YELLOW"));
+        }
+        else if(color.equals("BLANCO")){
+            back.setBackgroundColor(Color.parseColor("WHITE"));
+        }
+        else if(color.equals("NEGRO")){
+            back.setBackgroundColor(Color.parseColor("BLACK"));
+        }
         else{
             back.setBackgroundColor(Color.parseColor(color));
         }
